0afe28455d276da32058713cf32c9efc6c0700fa
Raphael Menezes <31518443+raphaelminacio@users.noreply.github.com>
2018-03-19 21:56:52 -0400

    Initial Commit

---
+++ Idade de Cachorro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Idade de Cachorro
+//
+//  Created by Ludimila Queiroz on 19/03/2018.
+//  Copyright © 2018 MoBI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
